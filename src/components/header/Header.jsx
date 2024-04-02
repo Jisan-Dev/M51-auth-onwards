eb53f6b91909455f6108130e228447df62475cc1
Jisun696 <istiakkashem35@gmail.com>
2024-04-03 04:58:04 +0600

    added header nav

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div>
-      <h1 className="text-center text-4xl font-bold">HEADER</h1>
+      <header className="container mx-auto">
+        <div className="navbar bg-base-100">
+          <div className="flex-1">
+            <a className="btn btn-ghost text-xl">AUTH ONWARDS_</a>
+          </div>
+          <div className="flex-none">
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/login">Login</NavLink>
+              </li>
+              <li>
+                <NavLink to="/register">Register</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
     </div>
   );
 };
